7cb34edb1005d27e04248478d770c3285b3f5206
Giovanni Maggi <gvnnmaggi@gmail.com>
2024-07-28 17:58:55 +0200

    added the loading button and revised the state variable names

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
 import Loader from './Loader.js'
+import GPXParser from 'gpxparser';
+import he from 'he';
 
-let gpx = {}
+let gpxInfo = {}
 let isLoading = false
-let stage = 'LoadGpx'
-let stageHistory = ['LoadGpx']
-let stages = ['LoadGpx','Converting','ShowingResult']
+let state = 'LoadGpx'
+let stateHistory = ['LoadGpx']
+let states = ['LoadGpx','Converting','ShowingResult']
 
 function App() {
   return (
@@ -21,23 +22,25 @@ class Router extends React.Component{
   constructor(props) {
     super(props)
     this.state = {
-      current : stage,
-      stageHistory : stageHistory,
+      current : state,
+      stageHistory : stateHistory,
     }
+    this.processGPX = this.processGPX.bind(this);
+    this.changeState = this.changeState.bind(this);
   }
 
-  changeStage(value) {
+  changeState(value) {
     if(value.current) {
-      stage = value.current
-      if(value.current === stages[0]) {
-        stageHistory = [stages[0]]
-      } else if(stageHistory[stageHistory.length - 1] !== value.current) {
-        stageHistory.push(value.current)
+      state = value.current
+      if(value.current === states[0]) {
+        stateHistory = [states[0]]
+      } else if(stateHistory[stateHistory.length - 1] !== value.current) {
+        stateHistory.push(value.current)
       }
     }
     this.setState({
-      current : stage,
-      stageHistory : stageHistory
+      current : state,
+      stateHistory : stateHistory
     })
   }
 
@@ -51,25 +54,56 @@ class Router extends React.Component{
       )
     } else {
       if(this.state.current === 'ShowingResult') {
-        // return (
-        //   <div className="translate-y">
-        //     <div className="button-login justify-center-column" onClick={() => {
-        //       window.location.href = stravaAuthorizeUrl
-        //     }}><p className="p-login p-login-or-size">LOGIN TO STRAVA</p></div>
-        //     <div className="margin-or">
-        //       <p className="p-or p-login-or-size">OR</p>
-        //     </div>
-        //     <div className="button-login justify-center-column" onClick={() => this.loadGPX()}>
-        //       <p className="p-login p-login-or-size">LOAD A GPX</p>
-        //       <input id="gpxInput" type="file" accept=".gpx" style={{display: 'none'}} onChange={this.processGPX} />
-        //     </div>
-        //   </div>
-        // )
-      } else if(this.state.current === 'LoadGpx') {
         return (
           <div>ECCOLO!</div>
         )
+      } else if(this.state.current === 'LoadGpx') {
+        return (
+          <div className="translate-y">
+            <div className="margin-or">
+              <p className="p-or p-login-or-size">CONVERT TO SVG</p>
+            </div>
+            <div className="button-login justify-center-column" onClick={() => this.loadGPX()}>
+              <p className="p-login p-login-or-size">LOAD A GPX</p>
+              <input id="gpxInput" type="file" accept=".gpx" style={{display: 'none'}} onChange={this.processGPX} />
+            </div>
+          </div>
+        )
       }
+    }
+  }
+
+  loadGPX() {
+    const gpxInput = document.getElementById('gpxInput')
+    if(gpxInput) gpxInput.click()
+  }
+
+  processGPX(event) {
+    if(event && event.target && event.target.files && event.target.files.length) {
+      isLoading = true
+      const file = event.target.files[0];
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        let gpxFile = e.target.result
+        const gpx = new GPXParser()
+        gpx.parse(gpxFile)
+        console.log('gpx.metadata.time: ', gpx.metadata.time)
+        console.log('gpx:', gpx)
+        console.log('unix time stamp in seconds', Math.floor(gpx.tracks[0].points[0].time)/1000)
+        const tracks = gpx.tracks.map(track => ({
+          altitudeStream: [...track.points.map(point => (point.ele))],
+          coordinates: track.points && track.points.length ? track.points.map(point => ([
+            point.lon,
+            point.lat
+          ])) : undefined,
+          timingStreamSeconds: track.points && track.points.length ? [...track.points.map(point => (Math.floor(point.time) / 1000))] : undefined,
+          name: track.name ? he.decode(track.name) : undefined,
+        }))
+        gpxInfo = tracks[0]
+        isLoading = false
+        this.changeState({current: 'ShowingResult'})
+      }
+      reader.readAsText(file);
     }
   }
 
